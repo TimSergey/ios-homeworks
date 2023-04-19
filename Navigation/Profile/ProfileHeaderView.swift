0c7d93953ee9a235dc4ae72cbb158760019a9e9f
Sergey Timofeev <tim.sergey90@mail.ru>
2023-04-19 21:28:43 +0300

    Made changes according to the expert's edits: Added an extension with enum Metric Brought textFIeld.text to the console I started writing commits in English :)

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -13,35 +13,36 @@ class ProfileHeaderView: UIView {
     
     private  let photoImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.image = UIImage(named: "avatar")
         imageView.layer.cornerRadius = 50
         imageView.layer.borderWidth = 3
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     private  let userNameLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.boldSystemFont(ofSize: 18)
         label.text = "Amazing wolf"
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private var statusTextLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 14)
         label.text = "Waiting for something..."
         label.textColor = .gray
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var statusButton: UIButton = {
         let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Show status", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .systemBlue
@@ -51,13 +52,13 @@ class ProfileHeaderView: UIView {
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOpacity = 0.7
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         return button
     }()
     
     private lazy var statusTextField: UITextField = {
         let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.font = UIFont.systemFont(ofSize: 15, weight: .regular)
         textField.backgroundColor = .white
         textField.layer.cornerRadius = 12
@@ -65,7 +66,8 @@ class ProfileHeaderView: UIView {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.placeholder = "Enter your status"
         textField.textAlignment = .center
-        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
+        textField.addTarget(self, action: #selector(hideKeyboard), for: .editingDidEndOnExit)
         
         return textField
     }()
@@ -85,35 +87,30 @@ class ProfileHeaderView: UIView {
         addSubview(statusTextLabel)
         addSubview(statusButton)
         addSubview(statusTextField)
-        
-        statusTextField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
-        statusTextField.addTarget(self, action: #selector(hideKeyboard), for: .editingDidEndOnExit)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        let indentConst:CGFloat = 16
-        
         NSLayoutConstraint.activate([
-            photoImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: indentConst),
-            photoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: indentConst),
+            photoImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: Metric.indentConst),
+            photoImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Metric.indentConst),
             photoImageView.heightAnchor.constraint(equalToConstant: 100),
             photoImageView.widthAnchor.constraint(equalToConstant: 100),
             
             userNameLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 27),
-            userNameLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: indentConst),
+            userNameLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: Metric.indentConst),
             
-            statusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: indentConst),
-            statusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -indentConst),
-            statusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: indentConst),
+            statusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Metric.indentConst),
+            statusButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -Metric.indentConst),
+            statusButton.topAnchor.constraint(equalTo: statusTextField.bottomAnchor, constant: Metric.indentConst),
             statusButton.heightAnchor.constraint(equalToConstant: 50),
             
-            statusTextLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: indentConst),
-            statusTextLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: indentConst),
+            statusTextLabel.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: Metric.indentConst),
+            statusTextLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: Metric.indentConst),
             
-            statusTextField.topAnchor.constraint(equalTo: statusTextLabel.bottomAnchor, constant: indentConst),
-            statusTextField.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: indentConst),
+            statusTextField.topAnchor.constraint(equalTo: statusTextLabel.bottomAnchor, constant: Metric.indentConst),
+            statusTextField.leadingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: Metric.indentConst),
             statusTextField.trailingAnchor.constraint(equalTo: statusButton.trailingAnchor),
             statusTextField.heightAnchor.constraint(equalToConstant: 40)
         ])
@@ -121,6 +118,7 @@ class ProfileHeaderView: UIView {
     
     @objc func buttonPressed() {
         statusTextLabel.text = statusText
+        print(statusTextField.text ?? "")
         statusTextField.text = ""
     }
     
@@ -130,5 +128,11 @@ class ProfileHeaderView: UIView {
     
     @objc func hideKeyboard() {
         endEditing(true)
+    }
+}
+
+extension ProfileHeaderView {
+    enum Metric {
+        static let indentConst:CGFloat = 16
     }
 }
